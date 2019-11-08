b8064031fe8a820d2fff3d4105510abd4816f136
ably77 <alexbrucely@gmail.com>
2019-11-08 08:50:42 -0800

    Updates

--- machineSets/setup_machineset.sh
+++ machineSets/setup_machineset.sh
@@ -10,4 +10,4 @@ ami_id=ami-01e7fdcb66157b224
 desired_replicas=2
 
 
-sed -e "s/<clusterID>/${clusterid}/g" -e "s/<role>/${role}/g" -e "s/<INSTANCETYPE>/${instancetype}/g" -e "s/<REGION>/${region}/g" -e "s/<ZONE>/${zone}/g" -e "s/<AMIID>/${ami_id}/g" -e "s/<REPLICAS>/${desired_replicas}/g" infra.machineset.template.yaml > $clusterid-$role-$region$zone.yaml
+sed -e "s/<clusterID>/${clusterid}/g" -e "s/<role>/${role}/g" -e "s/<INSTANCETYPE>/${instancetype}/g" -e "s/<REGION>/${region}/g" -e "s/<ZONE>/${zone}/g" -e "s/<AMIID>/${ami_id}/g" -e "s/<REPLICAS>/${desired_replicas}/g" machineset.template.yaml > $clusterid-$role-$region$zone.yaml

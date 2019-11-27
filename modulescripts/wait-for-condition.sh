25ae7defdee548d72860ffb5f25b197bc0d912b9
ably77 <alexbrucely@gmail.com>
2019-11-27 13:09:30 -0800

    Updates

--- modulescripts/wait-for-condition.sh
+++ modulescripts/wait-for-condition.sh
@@ -7,7 +7,7 @@ namespace=$2
 
 while [ "$OUTPUT" -ne 1 ]; do
   hashed_name=$(oc get pods -n ${namespace} | grep ${pod_name}  | awk 'NR==1{ print $1 }')
-  OUTPUT=`oc wait --for=condition=Ready pod/${hashed_name} -n ${namespace} --timeout=300s | grep -c "condition met"`;
+  OUTPUT=`oc wait --for=condition=Ready pod/${hashed_name} -n ${namespace} --timeout=300s 2>/dev/null | grep -c "condition met"`;
   seconds=$((seconds+5))
   printf "Waiting %s seconds for ${pod_name} to come up.\n" "${seconds}"
   sleep 5

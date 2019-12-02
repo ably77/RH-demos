56fac4d88b3e288272b4e90931c1809761df810b
ably77 <alexbrucely@gmail.com>
2019-12-02 12:06:02 -0800

    Update uninstall.sh

--- argocd/uninstall.sh
+++ argocd/uninstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-namespace=argocd
+argo_namespace="argocd"
+argo_route="argocd-server"
+app_name="simple-app"
 
-oc delete -f https://raw.githubusercontent.com/argoproj/argo-cd/v1.2.2/manifests/install.yaml -n ${namespace}
+# delete app
+argocd app delete ${app_name}
 
-oc delete routes argocd-server
+oc delete -f https://raw.githubusercontent.com/argoproj/argo-cd/v1.2.2/manifests/install.yaml -n ${argo_namespace}
+
+oc delete routes ${argo_route}

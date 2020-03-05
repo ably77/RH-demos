f55b97d8865e11282982e5e4054b9322d3829612
ably77 <alexbrucely@gmail.com>
2020-03-05 13:05:19 -0800

    Update

--- installer/azure/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir $HOME/Desktop/ly-demo-azure
-
-cp $HOME/Desktop/installer/azure/install-config-azure.yaml $HOME/Desktop/ly-demo-azure/install-config.yaml
-
-./openshift-install_4.2.2 create cluster --dir=$HOME/Desktop/ly-demo-azure --log-level debug
-
-git clone https://github.com/ably77/RH-demos.git $HOME/Desktop/ly-demo-azure/RH-Demos
